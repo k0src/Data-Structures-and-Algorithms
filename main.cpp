7fd725b9985bf36b71812fd62147c0e6bcf1450b
k0 <k0@k0src.com>
2024-04-30 16:08:16 -0500

    c

--- main.cpp
+++ main.cpp
@@ -8,21 +8,6 @@
 
 using namespace std;
 
-// commit to the hub
-
 int main()
 {
-    LinkedList l;
-    l.addLast(10);
-    l.addLast(20);
-    l.addLast(30);
-    l.addLast(40);
-    l.addLast(50);
-    l.insertAt(1, 99);
-    
-    for (int i = 0; i < 4; i++) {
-        cout << l.toArray()[i] << endl;
-    }
-
-    cout << l.middle();
 }
