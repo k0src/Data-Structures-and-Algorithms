0b84d3d104feaa5aff7f0599162679ef14c852f0
k0 <k0src@null.net>
2024-06-10 14:12:18 -0500

    searcher class

---
+++ Searcher.h
@@ -0,0 +1,20 @@
+#ifndef STRUCTURESALGOS_SEARCHER_H
+#define STRUCTURESALGOS_SEARCHER_H
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Searcher {
+public:
+    int linearSearch(const vector<int>& array, int target);
+    int binarySearchRecursive(const vector<int>& array, int start, int end, int target);
+    int binarySearchIterative(const vector<int>& array, int target);
+    int ternarySearch(vector<int>& array, int start, int end, int target);
+    int jumpSearch(vector<int>& array, int target);
+    int exponentialSearch(vector<int>& array, int target);
+};
+
+#endif //STRUCTURESALGOS_SEARCHER_H
