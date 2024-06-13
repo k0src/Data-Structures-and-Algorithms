aade8031055bb9709c0af83f1367fc454175b80d
k0 <k0src@null.net>
2024-06-13 18:32:05 -0500

    string manip

--- Searcher.h
+++ Searcher.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include <algorithm>
 
 using namespace std;
--- main.cpp
+++ main.cpp
@@ -32,7 +32,55 @@
 
 using namespace std;
 
+bool isVowel(const char& c) {
+    set<char> vowels {'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'};
+    return vowels.find(c) != vowels.end();
+}
+
+int countVowels(const string& str) {
+    int count = 0;
+    for (const char& c : str)
+        if (isVowel(c))
+            count++;
+    return count;
+}
+
+string reverseStringStack(const string& str) {
+    stack<char> s;
+    string result;
+    for (const char& c : str)
+        s.push(c);
+
+    while (!s.empty()) {
+        result.push_back(s.top());
+        s.pop();
+    }
+    return result;
+}
+
+string reverseStringBack(const string& str) {
+    string result;
+    for (int i = str.size() - 1; i >= 0; i--)
+        result.push_back(str[i]);
+    return result;
+}
+
+string reverseWords(const string& str) {
+    string result;
+    int j = str.size();
+    for (int i = str.size() - 1; i >= 0; i--) {
+        if (str[i] == ' ' || i == 0) {
+            int start = (i == 0) ? 0 : i + 1;
+            int len = j - start;
+            result.append(str.substr(start, len) + " ");
+            j = i;
+        }
+    }
+    return result;
+}
+
 int main()
 {
+    cout << reverseWords("hello world this is a string");
     return 0;
 }
